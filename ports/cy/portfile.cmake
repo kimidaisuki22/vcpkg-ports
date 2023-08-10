f948b01e1b286540d4932479216a4f64bd1a23a5
kimidaisuki22 <40718043+kimidaisuki22@users.noreply.github.com>
2023-08-10 20:06:27 +0800

    add cy lib.

---
+++ ports/cy/portfile.cmake
@@ -0,0 +1,20 @@
+
+vcpkg_from_git(
+    OUT_SOURCE_PATH SOURCE_PATH
+    URL https://github.com/kimidaisuki22/cy
+    REF f09f0390b1de039efbea5f6b8850656c43dab524
+	)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DBUILD_TESTING=OFF
+)
+
+vcpkg_cmake_install()
+vcpkg_copy_pdbs()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/CMakeLists.txt")
